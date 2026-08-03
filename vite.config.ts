263bbbef034674cdf582c476ff5bb177b648c778
Jesse Simmons <jesse.simmons@toyota.com>
2026-08-02 20:53:09 -0500

    Build development pages in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        development: 'development/index.html',
+        steamAuthConnectionGrants: 'development/steam-auth-connection-grants/index.html',
+      },
+    },
+  },
 })
